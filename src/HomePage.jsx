5dfecee7b86fa845bd9606c8ef85ed2762a0b3e2
naitsircnat <cristiangerardchen@gmail.com>
2025-01-11 12:43:09 +0000

    Added product cards using map and list rendering from json

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -1,7 +1,40 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import ProductCard from "./ProductCard.jsx";
+import axios from "axios";
 
 export default function HomePage() {
+  const [featuredProducts, setFeaturedProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchFeaturedProducts = async () => {
+      try {
+        const response = await axios.get("/featured.json");
+        setFeaturedProducts(response.data);
+      } catch (error) {
+        console.error("Error fetching products:", error);
+      }
+    };
+
+    fetchFeaturedProducts();
+  }, []);
+
+  const genProductCards = () => {
+    const productCards = [];
+
+    for (let product of featuredProducts) {
+      productCards.push(
+        <ProductCard
+          imageUrl={product.image}
+          price={product.price}
+          productName={product.name}
+          productDescript={product.description}
+        />
+      );
+    }
+
+    return productCards;
+  };
+
   return (
     <>
       {/* hero */}
@@ -12,26 +45,7 @@ export default function HomePage() {
       <section className="container my-5 px-lg-1">
         <h2>Our Coffee Blends</h2>
 
-        <div className="row gy-3">
-          <ProductCard
-            imageUrl="card-image.jpg"
-            price="18.99"
-            productName="test"
-            productDescript="hello"
-          />
-          <ProductCard
-            imageUrl="card-image.jpg"
-            price="18.99"
-            productName="test"
-            productDescript="hello"
-          />
-          <ProductCard
-            imageUrl="card-image.jpg"
-            price="18.99"
-            productName="test"
-            productDescript="hello"
-          />
-        </div>
+        <div className="row gy-3">{genProductCards()}</div>
       </section>
 
       {/* tea */}
@@ -39,24 +53,14 @@ export default function HomePage() {
         <h2>Our Tea Blends</h2>
 
         <div className="row gy-3">
-          <ProductCard
-            imageUrl="card-image.jpg"
-            price="18.99"
-            productName="test"
-            productDescript="hello"
-          />
-          <ProductCard
-            imageUrl="card-image.jpg"
-            price="18.99"
-            productName="test"
-            productDescript="hello"
-          />
-          <ProductCard
-            imageUrl="card-image.jpg"
-            price="18.99"
-            productName="test"
-            productDescript="hello"
-          />
+          {featuredProducts.map((product) => (
+            <ProductCard
+              imageUrl={product.image}
+              price={product.price}
+              productName={product.name}
+              productDescript={product.description}
+            />
+          ))}
         </div>
       </section>
     </>
