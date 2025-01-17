e77963aa009b3aff5851691c9e16b7464e114105
naitsircnat <cristiangerardchen@gmail.com>
2025-01-17 08:21:45 +0000

    Created cart store

---
+++ src/CartStore.js
@@ -0,0 +1,36 @@
+/*
+- imports
+- create cart atom
+- create initial card
+- create custom hook
+*/
+
+import { atom, useAtom } from "jotai";
+
+const initialCart = {
+  id: 1,
+  product_id: 1,
+  quantity: 10,
+  productName: "Organic Green Tea",
+  price: 12.99,
+  imageUrl: "https://picsum.photos/id/225/300/200",
+  description:
+    "Premium organic green tea leaves, rich in antioxidants and offering a smooth, refreshing taste.",
+};
+
+const cartAtom = atom(initialCart);
+
+export const useCart = () => {
+  const [cart, setCart] = useAtom(cartAtom);
+
+  const getCartTotal = () => {
+    return cart
+      .reduce((total, item) => total + item.price * item.quantity, 0)
+      .toFixed(2);
+  };
+
+  return {
+    cart,
+    getCartTotal,
+  };
+};
