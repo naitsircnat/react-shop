e1fd35b961574eb30894ba154c5186f63c13fcf8
naitsircnat <cristiangerardchen@gmail.com>
2025-01-24 12:20:13 +0000

    Added formik form code

--- src/Login.jsx
+++ src/Login.jsx
@@ -19,10 +19,88 @@ export default function Login() {
     password: Yup.string().required("Required"),
   });
 
+  const handleSubmit = async (values, actions) => {
+    try {
+      const response = await axios.post(
+        import.meta.env.VITE_API_URL + "/api/users/login",
+        values
+      );
+      console.log("Login successful", response.data);
+      actions.setSubmitting(false);
+      showMessage("Login successful", "success");
+      setLocation("/");
+    } catch (error) {
+      console.error("Login error:", error);
+      actions.setErrors({ submit: error.response.data.message });
+      actions.setSubmitting = false;
+    }
+  };
+
   return (
     <>
-      <h2>Login</h2>
-      <p>Login form</p>
+      <div className="container mt-5">
+        <p>Login form</p>
+        <Formik
+          initialValues={initialValues}
+          validationSchema={validationSchema}
+          onSubmit={handleSubmit}
+        >
+          {function (formik) {
+            return (
+              <Form>
+                <div className="mb-3">
+                  <label htmlFor="email" className="form-label">
+                    Email
+                  </label>
+                  <Field
+                    type="email"
+                    id="email"
+                    name="email"
+                    className="form-control"
+                  />
+                  <ErrorMessage
+                    name="email"
+                    component="div"
+                    className="text-danger"
+                  />
+                </div>
+
+                <div className="mb-3">
+                  <label htmlFor="password" className="form-label">
+                    Password
+                  </label>
+                  <Field
+                    type="password"
+                    id="password"
+                    name="password"
+                    className="form-control"
+                  />
+
+                  <ErrorMessage
+                    name="password"
+                    component="div"
+                    className="text-danger"
+                  />
+                </div>
+
+                {formik.errors.submit && (
+                  <div className="alert alert-danger">
+                    {formik.errors.submit}
+                  </div>
+                )}
+
+                <button
+                  type="submit"
+                  className="btn btn-primary"
+                  disabled={formik.isSubmitting}
+                >
+                  {formik.isSubmitting ? "Logging in..." : "Login"}
+                </button>
+              </Form>
+            );
+          }}
+        </Formik>
+      </div>
     </>
   );
 }
