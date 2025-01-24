7067396f995f27a13c8a6333f8a9b74a54b63ffd
naitsircnat <cristiangerardchen@gmail.com>
2025-01-24 08:00:08 +0000

    Added prelim setup for login page

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,6 +1,19 @@
 import React from "react";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
+import { useLocation } from "wouter";
+import axios from "axios";
+import { useFlashMessage } from "./FlashMessageStore";
 
 export default function Login() {
+  const { showMessage } = useFlashMessage;
+  const [, setLocation] = useLocation;
+
+  const initialValues = {
+    email: "",
+    password: "",
+  };
+
   return (
     <>
       <h2>Login</h2>
