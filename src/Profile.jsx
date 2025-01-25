81c5aabfd140941c4835d6f064d05cbc163e7c9d
naitsircnat <cristiangerardchen@gmail.com>
2025-01-25 09:09:58 +0000

    Added useEffect for Profile page

--- src/Profile.jsx
+++ src/Profile.jsx
@@ -4,3 +4,26 @@ import { useFlashMessage } from "./FlashMessageStore";
 import { useJwt } from "./UserStore";
 import * as Yup from "yup";
 import { Formik, ErrorMessage, Form, Field } from "formik";
+
+export default function Profile() {
+  const { showMessage } = useFlashMessage();
+  const { getJwt } = useJwt();
+  const [initialValues, setInitialValues] = useState({});
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const token = getJwt();
+      const response = await axios.get(
+        import.meta.env.VITE_API_URL + "/api/users/me",
+        {
+          headers: {
+            Authorization: `Bearer: ${token}`,
+          },
+        }
+      );
+      setInitialValues(response.data.user);
+    };
+
+    fetchData();
+  }, []);
+}
