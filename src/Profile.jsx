6e0fba6b0221cadf3b514a6cdbf4a6a0b5e22dae
naitsircnat <cristiangerardchen@gmail.com>
2025-01-25 11:02:57 +0000

    Added form validation and button handlers for Profile

--- src/Profile.jsx
+++ src/Profile.jsx
@@ -4,11 +4,13 @@ import { useFlashMessage } from "./FlashMessageStore";
 import { useJwt } from "./UserStore";
 import * as Yup from "yup";
 import { Formik, ErrorMessage, Form, Field } from "formik";
+import { useLocation } from "wouter";
 
 export default function Profile() {
   const { showMessage } = useFlashMessage();
   const { getJwt } = useJwt();
   const [initialValues, setInitialValues] = useState({});
+  const [, setLocation] = useLocation();
 
   useEffect(() => {
     const fetchData = async () => {
@@ -26,4 +28,51 @@ export default function Profile() {
 
     fetchData();
   }, []);
+
+  const formValidation = Yup.object({
+    name: Yup.string().required("Required"),
+    email: Yup.string().email("Invalid email address").required("Required"),
+    salutation: Yup.string(),
+    country: Yup.string(),
+    marketingPreferences: Yup.array().of(Yup.string()),
+  });
+
+  const submitHandler = async (actions, values) => {
+    try {
+      const token = getJwt();
+
+      if (!token) {
+        showMessage("You must be logged in", "danger");
+        actions.setSubmitting(false);
+        return;
+      }
+
+      await axios.put(import.meta.env.VITE_API_URL + "/api/users/me", values, {
+        headers: {
+          Authorization: `Bearer: ${token}`,
+        },
+      });
+
+      showMessage("Details updated successfully", "success");
+      actions.setSubmitting(false);
+    } catch (error) {
+      console.error("Error updating details: ", error);
+      actions.setErrors({
+        submit: error.response?.data?.message || "An error ocurred",
+      });
+      actions.setSubmitting(false);
+    }
+  };
+
+  const deleteHandler = async () => {
+    const token = getJwt();
+
+    await axios.delete(import.meta.env.VITE_API_URL + "/api/users/me", {
+      headers: {
+        Authorization: `Bearer: ${token}`,
+      },
+    });
+    showMessage("Account deleted", "danger");
+    setLocation("/");
+  };
 }
